93776d1a56bc075e64adb949ba60b271d0105695
jonghyeon <whdgusdldl22@naver.com>
2024-04-13 14:14:25 +0900

    연락처 영역 완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import { contactTitle, contactText } from "@/constants";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">{contactTitle.title}</h2>
+        <div className="contact__lines top" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">
+            <div>
+              <a href={contactText.link} target="_blank">
+                {contactText.title}
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="contact__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
